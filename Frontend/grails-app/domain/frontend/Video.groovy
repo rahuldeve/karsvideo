da6de11d09a017998225a68bcd0890061e7e32e9
rahuldeve <rahul.e.dev@gmail.com>
2015-06-13 15:18:12 +0530

    add Video domain class
    Signed-off-by: rahuldeve <rahul.e.dev@gmail.com>

---
+++ Frontend/grails-app/domain/frontend/Video.groovy
@@ -0,0 +1,13 @@
+package frontend
+
+class Video {
+
+    String name
+    String location
+    String keyphrases
+    int relevance
+    String relevance_group
+
+    static constraints = {
+    }
+}
