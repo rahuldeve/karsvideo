8f51b2feacd07a209bdc3cc127db922dea0007b9
rahuldeve <rahul.e.dev@gmail.com>
2015-06-22 11:30:45 +0530

    update javascript file for result page
    Signed-off-by: rahuldeve <rahul.e.dev@gmail.com>

--- Frontend/web-app/js/asd.js
+++ Frontend/web-app/js/asd.js
@@ -7,26 +7,82 @@
 
 // http://packery.metafizzy.co/packery.pkgd.js added as external resource
 
-docReady( function() {
-  var container = document.querySelector('.packery');
-  var pckry = new Packery( container, { gutter: 5} );
-  
-  eventie.bind( container, 'click', function( event ) {
-    // don't proceed if item content was not clicked on
-    var target = event.target;
-    if ( !classie.has( target, 'item-content' )  ) {
-      return;
-    }
-    var itemElem = target.parentNode;
-    var isExpanded = classie.has( itemElem, 'is-expanded' );
-    classie.toggleClass( itemElem, 'is-expanded' );
-  
-    if ( isExpanded ) {
-      // if shrinking, just layout
-      pckry.layout();
-    } else {
-      // if expanding, fit it
-      pckry.fit( itemElem );
-    }
-  });
+$( function() {
+    var $container = $('.packery').packery({
+        gutter:10
+    });
+
+    $container.find('.item').each( function( i, itemElem ) {
+        // make element draggable with Draggabilly
+        var draggie = new Draggabilly( itemElem );
+        // bind Draggabilly events to Packery
+        $container.packery( 'bindDraggabillyEvents', draggie );
+    });
+
+    $container.on( 'click', '.item-content', function( event ) {
+        var target = event.target;
+        var $target = $( target );
+
+
+
+
+        // disable transition
+        //$target.css( transitionProp, 'none' );
+        // set current size
+        //$target.css({
+        //  width: $target.width(),
+        //  height: $target.height()
+        // });
+
+        var $itemElem = $target.parent();
+        var isExpanded = $itemElem.hasClass('is-expanded');
+        $itemElem.toggleClass('is-expanded');
+
+        //var container_first_row = $('.container-first-row');
+        var container_first_row = $itemElem.find('.container-first-row');
+        var segbar=$itemElem.find('.segbar');
+        var container_third_row = $itemElem.find('.container-third-row');
+
+        if(isExpanded){
+            container_first_row.children().removeClass('expand');
+            segbar.removeClass('expand');
+            container_third_row.removeClass('expand');
+        }
+
+        else{
+            container_first_row.children().addClass('expand');
+            segbar.addClass('expand');
+            container_third_row.addClass('expand');
+        }
+
+
+        //console.log(content.children.toggleClass('is_expanded'));
+        // force redraw
+        //var redraw = target.offsetWidth;
+        // renable default transition
+        // target.style[ transitionProp ] = '';
+
+        // reset 100%/100% sizing after transition end
+        /* if ( transitionProp ) {
+         $target.one( transitionEndEvent, function() {
+         target.style.width = '';
+         target.style.height = '';
+         });
+         }
+         */
+        // set new size
+        /*$target.css({
+         width: $itemElem.width(),
+         height: $itemElem.height()
+         });*/
+
+        if ( isExpanded ) {
+            // if shrinking, just layout
+            $container.packery();
+        } else {
+            // if expanding, fit it
+            $container.packery( 'fit', $itemElem[0] );
+        }
+
+    });
 });
