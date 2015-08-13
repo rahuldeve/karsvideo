d44815056ea8525152ed730d1fa2176426d32a2a
rahuldeve <rahul.e.dev@gmail.com>
2015-08-13 22:10:04 +0530

    refactor some packages
    Signed-off-by: rahuldeve <rahul.e.dev@gmail.com>

---
+++ Index/src/com/company/Index/Retriever/Datastructures/Tupule.java
@@ -0,0 +1,11 @@
+package com.company.Index.Retriever.Datastructures;
+
+/**
+ * Created by rahul on 12/08/15.
+ */
+public class Tupule {
+
+    public String segid;
+    public int relevance;
+
+}
---
+++ Index/src/com/company/Index/Retriever/IndexRetriever.java
@@ -0,0 +1,7 @@
+package com.company.Index.Retriever;
+
+/**
+ * Created by rahul on 3/08/15.
+ */
+public class IndexRetriever {
+}
