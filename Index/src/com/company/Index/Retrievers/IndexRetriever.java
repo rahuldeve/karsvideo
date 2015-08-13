9efbce0795293496f671f8918b371bcf7e9ba8e9
rahuldeve <rahul.e.dev@gmail.com>
2015-08-13 22:11:40 +0530

    refactor changes
    Signed-off-by: rahuldeve <rahul.e.dev@gmail.com>

--- Index/src/com/company/Index/Retrievers/IndexRetriever.java
+++
@@ -1,7 +0,0 @@
-package com.company.Index.Retrievers;
-
-/**
- * Created by rahul on 3/08/15.
- */
-public class IndexRetriever {
-}
--- Index/src/com/company/Index/Retrievers/InvertedIndex.java
+++
@@ -1,7 +0,0 @@
-package com.company.Index.Retrievers;
-
-/**
- * Created by rahul on 11/06/15.
- */
-public class InvertedIndex {
-}
