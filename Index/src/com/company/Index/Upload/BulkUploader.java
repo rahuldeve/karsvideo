73c2bfdd58b76bcd5af8b6f250a669316fdc1750
rahuldeve <rahul.e.dev@gmail.com>
2015-08-13 22:12:07 +0530

    updae bulk uploader
    Signed-off-by: rahuldeve <rahul.e.dev@gmail.com>

--- Index/src/com/company/Index/Upload/BulkUploader.java
+++ Index/src/com/company/Index/Upload/BulkUploader.java
@@ -3,15 +3,34 @@ package com.company.Index.Upload;
 import com.mongodb.client.MongoCollection;
 import org.bson.Document;
 
+import java.io.File;
+import java.io.FilenameFilter;
+
 /**
  * Created by rahul on 30/07/15.
  */
 public class BulkUploader extends Uploader {
 
-    MongoCollection<Document> videocollection;
 
     public BulkUploader(MongoCollection<Document> videocollection){
         super(videocollection);
+    }
+
+    public void scanFolder(String basePath){
+
+        File baseFolder = new File(basePath);
+
+        String folders[] = baseFolder.list(new FilenameFilter() {
+            @Override
+            public boolean accept(File file, String s) {
+                return new File(file,s).isDirectory();
+            }
+        });
+
+
+        for(String folder : folders)
+            add(folder);
+
     }
 
 }
